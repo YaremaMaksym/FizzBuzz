de5242835bb21eea3ace9420ea00f654a10ad42b
Maksym Yarema <macsym2@gmail.com>
2024-02-12 15:57:26 +0200

    feat(Main): add func() to test rules

--- src/main/java/org/yaremax/Main.java
+++ src/main/java/org/yaremax/Main.java
@@ -1,7 +1,33 @@
 package org.yaremax;
 
+import org.yaremax.rule.BuzzRule;
+import org.yaremax.rule.FizzRule;
+
+import java.util.ArrayList;
+import java.util.List;
+
 public class Main {
+    static FizzRule fizzRule = new FizzRule();
+    static BuzzRule buzzRule = new BuzzRule();
+
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        System.out.println(func(20));
+    }
+
+    public static List<String> func(int n) {
+        List<String> arr = new ArrayList<>();
+        StringBuilder sb = new StringBuilder();
+
+        for (int i = 1; i <= n; i++) {
+            sb.delete(0, sb.length());
+
+            sb.append(fizzRule.doRule(i));
+            sb.append(buzzRule.doRule(i));
+
+            if (sb.isEmpty()) sb.append(i);
+
+            arr.add(sb.toString());
+        }
+        return arr;
     }
 }
