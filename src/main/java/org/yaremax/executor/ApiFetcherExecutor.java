b5eb5850245ad2e4c477f41920ec6a194c0eed0a
Maksym Yarema <macsym2@gmail.com>
2024-02-15 14:09:15 +0200

    feat: add ApiFetcherExecutor

---
+++ src/main/java/org/yaremax/executor/ApiFetcherExecutor.java
@@ -0,0 +1,10 @@
+package org.yaremax.executor;
+
+import java.util.List;
+
+public class ApiFetcherExecutor implements Executor<Void> {
+    @Override
+    public List<String> execute(Void unused) {
+        return null;
+    }
+}
